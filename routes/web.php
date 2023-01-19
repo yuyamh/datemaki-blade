fe2440c8c86e8176f9d347569ea1410b8db5cba6
yuyamh <yuya.htr0828@gmail.com>
2023-01-19 17:01:44 +0900

    教案一覧画面のルーティング、コントローラ設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+// Route::resource('posts', PostController::class);
 
 require __DIR__.'/auth.php';
