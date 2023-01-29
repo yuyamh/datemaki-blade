c9def6494e16b2fa9e3d049b311d3b31b6f9d2a1
yuyamh <yuya.htr0828@gmail.com>
2023-01-29 22:09:18 +0900

    footerの作成

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="pt-4 bg-white">
+    <footer class="px-4 mx-auto max-w-screen-2xl">
+        <div class="flex flex-col items-center pt-4">
+            <nav class="flex flex-wrap justify-center md:justify-start gap-x-4 gap-y-2 md:gap-6">
+                <a href="#" class="text-gray-500 transition duration-100 hover:text-orange-500 active:text-orange-600">ホーム</a>
+                <a href="#" class="text-gray-500 transition duration-100 hover:text-orange-500 active:text-orange-600">だてまきとは</a>
+                <a href="#" class="text-gray-500 transition duration-100 hover:text-orange-500 active:text-orange-600">利用規約</a>
+                <a href="#" class="text-gray-500 transition duration-100 hover:text-orange-500 active:text-orange-600">プライバシーポリシー</a>
+                <a href="#" class="text-gray-500 transition duration-100 hover:text-orange-500 active:text-orange-600">お問い合わせ</a>
+            </nav>
+        </div>
+    <div class="py-6 text-sm text-center text-gray-400">© 2023 - Yuya M All rights reserved.</div>
+    </footer>
+</div>
