23ec9c3d22c65575fbb7d24abfdfec0142eece5f
yuyamh <yuya.htr0828@gmail.com>
2023-02-16 17:45:36 +0900

    細かな部分のリファクタリング＆レイアウト変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,41 +1,48 @@
 <x-app-layout>
-    <x-slot name="header">教案一覧</x-slot>
-    <x-slot name="title">みんなの教案</x-slot>
+    <x-slot name="header">じぶんの教案一覧</x-slot>
+    <x-slot name="title">じぶんの教案</x-slot>
     <div class="flex flex-col p-2 md:py-10">
         <div class="overflow-x-auto md:overflow-x-hidden">
-            <table class="border-b-2 table-fixed lg:w-full table-index">
-                <thead class="bg-orange-300 border-b-2 border-gray-500 ">
-                    <tr class="text-sm lg:text-lg">
-                        <div class="font-bold">
-                            <th scope="col" class="w-1/12">♯</th>
-                            <th scope="col" class="w-3/12 lg:w-5/12">タイトル</th>
-                            <th scope="col" class="w-2/12 lg:w-2/12">レベル</th>
-                            <th scope="col" class="w-2/12">投稿者</th>
-                            <th scope="col" class="w-2/12">投稿日</th>
+            @if ($posts->isEmpty())
+                <div class="py-12">
+                    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                        <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <p>教案が投稿されていません。</p>
+                            </div>
                         </div>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($posts as $post)
-                        @if ($loop->index % 2 === 0)
-                        <tr class="bg-gray-100 border-b hover:cursor-pointer hover:bg-yellow-100" data-href="{{ route('posts.show', $post) }}">
-                        @else
-                        <tr class="bg-white border-b hover:cursor-pointer hover:bg-yellow-100" data-href="{{ route('posts.show', $post) }}">
-                        @endif
+                    </div>
+                </div>
+            @else
+                <table class="border-b-2 table-fixed md:w-full table-index">
+                    <thead class="bg-orange-300 border-b-2 border-gray-500">
+                        <tr>
+                            <div class="font-bold">
+                                <th scope="col" class="w-1/12">♯</th>
+                                <th scope="col" class="w-3/12 lg:w-5/12">タイトル</th>
+                                <th scope="col" class="w-2/12 lg:w-2/12">レベル</th>
+                                <th scope="col" class="w-2/12">投稿者</th>
+                                <th scope="col" class="w-2/12">投稿日</th>
+                            </div>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            @if ($loop->index % 2 === 0)
+                            <tr class="bg-gray-100 border-b hover:cursor-pointer hover:bg-yellow-100" data-href="{{ route('posts.show', $post) }}">
+                            @else
+                            <tr class="bg-white border-b hover:cursor-pointer hover:bg-yellow-100" data-href="{{ route('posts.show', $post) }}">
+                            @endif
                                 <td class="font-medium truncate">{{ $loop->index + 1 }}</td>
                                 <td class="font-light truncate">{{ $post->title }}</td>
                                 <td class="font-light truncate">{{ $post->level }}</td>
                                 <td class="font-light truncate">{{ $post->user->name }}</td>
                                 <td class="font-light truncate">{{ $post->created_at->format('Y/m/d') }}</td>
-                        </tr>
-                    @empty
-                    <tr>
-                        <td></td>
-                        <td>まだ教案が投稿されていません。</td>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
         </div>
         <div class="m-4">{{ $posts->onEachSide(1)->links() }}</div>
     </div>
