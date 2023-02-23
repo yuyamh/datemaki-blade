3fe4f0d12d6bbd088129c310e0150802ead131ed
yuyamh <yuya.htr0828@gmail.com>
2023-02-23 22:08:02 +0900

    教案一覧画面のレイアウトをカード型へ変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::latest()->paginate(20);
+        $posts = Post::latest()->paginate(21);
         $data = ['posts' => $posts];
         return view('posts.index', $data);
     }
