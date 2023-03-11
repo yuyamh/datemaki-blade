17cffbc17bb91671680f49922a1c808d77bea79b
yuyamh <yuya.htr0828@gmail.com>
2023-03-11 18:12:13 +0900

    教案一覧画面、ブックマークした教案に印をつけた。

--- resources/views/components/index-cards.blade.php
+++ resources/views/components/index-cards.blade.php
@@ -12,20 +12,25 @@
 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:mr-6">
     @foreach ($posts as $post)
     <div class="flex flex-col items-start justify-center w-full p-3 bg-white rounded-lg cursor-pointer hover:duration-200 sahdow-lg card hover:scale-95 hover:transition-all hover:bg-yellow-50" data-href="{{ route('posts.show', $post) }}">
-        <div class="flex justify-start w-full mb-4">
-            <img class="w-12 h-12 bg-gray-200 rounded-full" src="{{ isset($post->user->profile_photo_path) ? asset('storage/' . $post->user->profile_photo_path) : asset('images/user_icon.png') }}" alt="photo">
-            <div class="ml-2 truncate">
-                <p class="text-sm text-gray-500 truncate">{{ $post->user->name }}</p>
-                <p class="mb-2 text-xl font-bold text-gray-600 truncate">{{ $post->title }}</p>
-                <p class="text-sm text-gray-600">
-                    @if(isset($post->text->text_name))
-                    <span>{{ $post->text->text_name }}&nbsp;/&nbsp;</span>
-                    @else
-                    <span>-&nbsp;/&nbsp;</span>
-                    @endif
-                    <span>{{ $post->level }}&nbsp;</span>
-                </p>
+        <div class="flex items-start justify-between w-full">
+            <div class="flex justify-start w-full mb-4">
+                <img class="w-12 h-12 bg-gray-200 rounded-full" src="{{ isset($post->user->profile_photo_path) ? asset('storage/' . $post->user->profile_photo_path) : asset('images/user_icon.png') }}" alt="photo">
+                <div class="ml-2 truncate">
+                    <p class="text-sm text-gray-500 truncate">{{ $post->user->name }}</p>
+                    <p class="mb-2 text-xl font-bold text-gray-600 truncate">{{ $post->title }}</p>
+                    <p class="text-sm text-gray-600">
+                        @if(isset($post->text->text_name))
+                        <span>{{ $post->text->text_name }}&nbsp;/&nbsp;</span>
+                        @else
+                        <span>-&nbsp;/&nbsp;</span>
+                        @endif
+                        <span>{{ $post->level }}&nbsp;</span>
+                    </p>
+                </div>
             </div>
+            @if (Auth::user()->is_bookmarked($post->id))
+                <span><i class="text-orange-400 fa-solid fa-bookmark"></i></i></span>
+            @endif
         </div>
         <p class="h-16 m-3 mt-0 text-base font-normal leading-snug text-gray-500 line-clamp-3">{{ $post->description }}</p>
         <p class="ml-auto text-xs font-light text-gray-400">{{ $post->updated_at->format('Y/m/d') }}</p>
