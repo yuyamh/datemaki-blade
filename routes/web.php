5d21dfe0d08f313078ad67822acd440ba796d84b
yuyamh <yuya.htr0828@gmail.com>
2023-05-11 23:27:03 +0900

    [feat]ホーム画面の実装

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
