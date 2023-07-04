7fa824080bce139714129dbc4e79cbd01aaf3a07
yuyamh <yuya.htr0828@gmail.com>
2023-07-04 15:23:10 +0900

    [feat]教案投稿画面、概要のモーダル上部に閉じるボタンを追加

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -45,12 +45,12 @@ $maxWidth = [
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="show"
-    class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+    class="fixed inset-0 z-50 px-4 py-6 overflow-y-auto sm:px-0"
     style="display: {{ $show ? 'block' : 'none' }};"
 >
     <div
         x-show="show"
-        class="fixed inset-0 transform transition-all"
+        class="fixed inset-0 transition-all transform"
         x-on:click="show = false"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0"
@@ -64,7 +64,7 @@ $maxWidth = [
 
     <div
         x-show="show"
-        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6  overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
@@ -72,6 +72,13 @@ $maxWidth = [
         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     >
-        {{ $slot }}
+        {{-- 閉じるボタン --}}
+        <div x-on:click="show = false">
+            {{ $close }}
+        </div>
+        {{-- モーダル本体 --}}
+        <div class="bg-white rounded-lg">
+            {{ $modal }}
+        </div>
     </div>
 </div>
