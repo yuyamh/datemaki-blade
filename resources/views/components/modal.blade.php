6a8817827f97d12751df8f822fe3145e0846d249
yuyamh <yuya.htr0828@gmail.com>
2023-07-04 16:28:28 +0900

    [feat]教案投稿画面、概要のモーダル上部に閉じるボタンを追加(再修正)

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -71,7 +71,7 @@ $maxWidth = [
 
     <div
         x-show="show"
-        class="mb-6  overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
@@ -79,13 +79,6 @@ $maxWidth = [
         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     >
-        {{-- 閉じるボタン --}}
-        <div x-on:click="show = false">
-            {{ $close }}
-        </div>
-        {{-- モーダル本体 --}}
-        <div class="bg-white rounded-lg">
-            {{ $modal }}
-        </div>
+        {{ $slot }}
     </div>
 </div>
