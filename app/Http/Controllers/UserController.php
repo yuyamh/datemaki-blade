df9bb2b7e09188188c190e7afe89b743c43aa108
yuyamh <yuya.htr0828@gmail.com>
2023-07-12 21:09:29 +0900

    [docs]PHPDocの修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
