3c0e86a64c91ef3ffc6c48ce6d10b7b2028f810d
yuyamh <yuya.htr0828@gmail.com>
2023-07-19 23:44:45 +0900

    [feat]検索フォームをヘッダーの下に移動

--- resources/views/posts/bookmarks.blade.php
+++ resources/views/posts/bookmarks.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
     <x-slot name="header">ブックマーク一覧</x-slot>
     <x-slot name="title">ブックマーク一覧</x-slot>
-    <x-index-cards :posts="$posts" message="まだブックマークしていません。" />
+    <x-index-cards :posts="$posts" message="まだブックマークしていません。" subtitle="ブックマークした教案一覧" />
 </x-app-layout>
