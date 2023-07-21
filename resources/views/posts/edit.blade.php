335e70a693a368f2f7bc93628b59e608180a1deb
yuyamh <yuya.htr0828@gmail.com>
2023-07-21 17:22:53 +0900

    Revert "[feat]検索フォームをヘッダーの下に移動"
    This reverts commit 3c0e86a64c91ef3ffc6c48ce6d10b7b2028f810d.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="w-full max-w-2xl mx-auto">
         <form action="{{ route('posts.update', $post) }}" method="POST" enctype="multipart/form-data">
             @method('PATCH')
-            <x-form :texts="$texts" :post="$post" subtitle="教案編集フォーム" />
+            <x-form :texts="$texts" :post="$post" />
             <div class="flex justify-center py-5">
                 <x-primary-button type="submit" class="mr-12 bg-orange-400 shadow-md hover:bg-orange-300 active:bg-orange-400 focus:bg-orange-400">更新する</x-primary-button>
                 <x-secondary-button onclick="location.href='{{ route('posts.show', $post) }}'">キャンセル</x-secondary-button>
