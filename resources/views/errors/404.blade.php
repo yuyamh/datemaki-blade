7bd6fd8f8c26fefcb5c8526d077dabb8341d445f
yuyamh <yuya.htr0828@gmail.com>
2023-08-07 19:36:36 +0900

    [feat]HTTPステータスコードのエラーメッセージ、独自のものを作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::layout')
+
+@section('title', __('Not Found'))
+@section('code', '404 NOT FOUND')
+@section('message', 'ページが見つかりませんでした。')
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::layout')
+
+@section('title', __('Service Unavailable'))
+@section('code', '503 Service Unavailable')
+@section('message', '申し訳ありません、このページを表示できませんでした。')
