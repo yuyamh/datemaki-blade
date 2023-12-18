3d291200e340777f4f88dde0bcbbc7a6c263c0a5
yuyamh <yuya.htr0828@gmail.com>
2023-12-18 12:50:11 +0900

    プロフィール画像アップロード用モーダルの設置

--- resources/views/components/picture-input.blade.php
+++ resources/views/components/picture-input.blade.php
@@ -1,4 +1,5 @@
-<div class="flex mb-4" x-data="imagePreview()">
+{{-- <div class="flex mb-4" x-data="imagePreview()"> --}}
+<div class="flex mb-4">
     <div class="mr-3">
         {{-- ゲストユーザーの場合は、指定した画像をプロフ画像として表示する。 --}}
         @if (Auth::check() && $user->role === 'guest')
@@ -14,11 +15,16 @@
         {{-- ゲストユーザーの場合は、プロフ画像を変更できない --}}
         @can ('update', $user)
         <button
-            x-on:click="document.getElementById('image').click()"
+            {{-- x-on:click="document.getElementById('image').click()" --}}
+            x-data=""
+            x-on:click.prevent="$dispatch('open-modal', 'modal-upload-profile-icon')"
             type="button"
             class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 uppercase bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-100 active:outline-none active:ring-2 active:ring-orange-400 active:ring-offset-2 active:text-gray-500 active:bg-gray-50 focus:ring-orange-400">
             アイコンを設定
         </button>
+        <x-modal name="modal-upload-profile-icon">
+            <x-modal-upload-profile-icon />
+        </x-modal>
         @else
         <div class="flex flex-col items-start">
             <button disabled
@@ -29,8 +35,8 @@
             <p class="mt-1 text-xs text-red-500">※変更不可</p>
         </div>
         @endcan
-        <input @change="showPreview(event)" type="file" name="image" id="image" class="hidden">
-        <script>
+        {{-- <input @change="showPreview(event)" type="file" name="image" id="image" class="hidden"> --}}
+        {{-- <script>
             function imagePreview() {
                 return {
                     showPreview: (event) =>{
@@ -41,6 +47,6 @@
                     }
                 }
              }
-        </script>
+        </script> --}}
     </div>
 </div>
