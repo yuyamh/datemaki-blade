c690473fc2ee3af328500cafd8a62c533609be60
yuyamh <yuya.htr0828@gmail.com>
2023-12-22 13:14:53 +0900

    モーダルのUIの大枠作成

--- resources/views/components/modal-upload-profile-icon.blade.php
+++ resources/views/components/modal-upload-profile-icon.blade.php
@@ -1,1 +1,11 @@
-
+<div class="flex items-center justify-center w-full">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full border-2 border-gray-400 border-dashed rounded-lg cursor-pointer h-96 bg-gray-50 hover:bg-gray-100 hover:border-orange-400 hover:border-4">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <i class="m-5 text-6xl text-gray-400 fa-solid fa-cloud-arrow-up"></i>
+            <p class="mb-2 text-sm text-gray-500">ここにファイルをドロップ</p>
+            <p class="mb-2 text-sm text-gray-500">またはクリックして画像選択</p>
+            <p class="text-xs text-gray-500">PNG, JPG or GIF（3KBまで）</p>
+        </div>
+        <input id="dropzone-file" type="file" class="hidden" />
+    </label>
+</div>
