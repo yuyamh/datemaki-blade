1e74d58e922055eaae361e4884465a96dffde55f
yuyamh <yuya.htr0828@gmail.com>
2023-12-25 23:08:11 +0900

    ドラッグ&ドロップ実装

--- public/js/profile_icon.js
+++ public/js/profile_icon.js
@@ -9,3 +9,38 @@ $('#iconInput').on('change', function (e) {
         $('#preview').attr('src', iconUrl);
     };
 });
+
+$('#isDesktop').on('click', function () {
+    if ($('#modal').hasClass('hidden')) {
+        $('#modal').removeClass('hidden');
+    }
+});
+
+// ドラッグ&ドロップ
+$(document).on('drop', '#dropzone', function (e) {
+    $(this).find('#uploader')[0].files = e.originalEvent.dataTransfer.files;
+    $(this).find('#uploader').trigger('change');
+});
+
+// モーダルから呼び出し元へデータの受け渡し
+$(document).on('change', '#uploader', function (e) {
+    let fileReader = new FileReader();
+    fileReader.onload = function () {
+        $('#iconInput').prop('files', e.target.files);
+        $('#preview').attr('src', fileReader.result);
+        $('#modal').addClass('hidden');
+    };
+
+    fileReader.readAsDataURL(e.target.files[0]);
+});
+
+$(function () {
+    $(document).on('dragover drop', function (e) {
+        //HTML自体にドラッグ＆ドロップしても遷移しないようにする
+        e.preventDefault();
+        e.stopPropagation();
+
+        // ドラッグ時のクラス追加
+        $('label[for="uploader"]').addClass('border-orange-400 border-4');
+    });
+});
--- resources/views/components/picture-input.blade.php
+++ resources/views/components/picture-input.blade.php
@@ -1,5 +1,5 @@
 <div class="flex mb-4">
-    <div class="mr-3">
+    <div class="flex items-center mr-3">
         {{-- ゲストユーザーの場合は、指定した画像をプロフ画像として表示する。 --}}
         @if (Auth::check() && $user->role === 'guest')
             <img src="{{ asset('images/guest_user_icon.png') }}" class="object-cover w-16 h-16 bg-gray-200 border-none rounded-full md:h-32 md:w-32" alt="photo">
@@ -10,7 +10,7 @@
                 class="object-cover w-16 h-16 bg-gray-200 border-none rounded-full md:h-32 md:w-32">
         @endif
     </div>
-    <div class="flex items-center">
+    <div class="flex flex-col items-start justify-center md:items-center md:flex-row">
         {{-- ゲストユーザーの場合は、プロフ画像を変更できない --}}
         @can ('update', $user)
         {{-- スマホ・タブレット版ボタン --}}
@@ -25,12 +25,22 @@
             x-data=""
             x-on:click.prevent="$dispatch('open-modal', 'modal-upload-profile-icon')"
             type="button"
+            id="isDesktop"
             class="items-center hidden px-4 py-2 text-sm font-medium text-gray-700 uppercase bg-white border border-gray-300 rounded-md shadow-sm lg:inline-flex hover:bg-gray-100 active:outline-none active:ring-2 active:ring-orange-400 active:ring-offset-2 active:text-gray-500 active:bg-gray-50 focus:ring-orange-400">
             アイコンを設定
         </button>
-        <x-modal name="modal-upload-profile-icon">
-            <x-modal-upload-profile-icon />
-        </x-modal>
+        <div id="modal">
+            <x-modal name="modal-upload-profile-icon">
+                <x-modal-upload-profile-icon />
+            </x-modal>
+        </div>
+        {{-- キャンセルボタン --}}
+        <button
+            id="cancelBtn"
+            type="button"
+            class="inline-flex items-center px-4 py-2 mt-1 text-sm font-medium text-gray-700 uppercase bg-white border border-gray-300 rounded-md shadow-sm md:ml-1 hover:bg-gray-100 active:outline-none active:ring-2 active:ring-orange-400 active:ring-offset-2 active:text-gray-500 active:bg-gray-50 focus:ring-orange-400 md:mt-0">
+            元に戻す
+        </button>
         @else
         <div class="flex flex-col items-start">
             <button disabled
